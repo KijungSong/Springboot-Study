aa1997551d6ce41fb35d5514e9e54f5e2bfcead3
KiJung Song <kijungsong@KiJungui-MacBook-Air.local>
2018-08-07 22:18:57 +0900

    thymeleaf 실습

---
+++ myshop/src/main/java/com/daebaksong/myshop/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.daebaksong.myshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/members")
+public class MemberController {
+
+    @GetMapping
+    public String members(ModelMap modelMap){
+        modelMap.addAttribute("count", 500);
+
+        return "members"; // view name을 리턴한다.
+    }
+}
