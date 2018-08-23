b9e258ab9fe97a6c2c4cc9d708bbe6af0ce58fce
Kijung Song <skj4522@gmail.com>
2018-08-23 18:32:29 +0900

    [myboard] joinform 예제 - 회원가입후 welcome 페이지로 이동.

---
+++ myboard/src/main/java/com/daebaksong/myboard/controller/MemberController.java
@@ -0,0 +1,43 @@
+package com.daebaksong.myboard.controller;
+
+import com.daebaksong.myboard.domin.Member;
+import com.daebaksong.myboard.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+@RequestMapping("/members")
+public class MemberController {
+    @Autowired
+    MemberService memberService;
+
+    @GetMapping("/joinform")
+    public String joinform() {
+
+        return "joinform";
+    }
+
+    @PostMapping("/join")
+    public String join(@ModelAttribute Member member) {
+        System.out.println("---------------- join");
+        System.out.println(member.getName());
+        System.out.println(member.getPasswd());
+        member.setRegdate(LocalDateTime.now());
+        memberService.addMember(member);
+
+        return "redirect:welcome";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
+    }
+
+
+}
