0ad1c27791c421ed9fa6bc054002e1dac0f0b0c1
Kijung Song <skj4522@gmail.com>
2018-08-28 16:14:45 +0900

    [myboard] /boards 요청에 대해 fetch join 적용.

--- myboard/src/main/java/com/daebaksong/myboard/repository/BoardRepository.java
+++ myboard/src/main/java/com/daebaksong/myboard/repository/BoardRepository.java
@@ -4,9 +4,12 @@ import com.daebaksong.myboard.domin.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     public Page<Board> findAllBy(Pageable pageable);
+    @Query(value = "select distinct b from Board b join fetch b.member order by b.id desc",
+            countQuery = "select count(b) from Board b")
     public Page<Board> findAllByOrderByIdDesc(Pageable pageable);
     public Page<Board> findAllByTitleContains(String title, Pageable pageable);
     public Page<Board> findAllByContentContains(String content, Pageable pageable);
