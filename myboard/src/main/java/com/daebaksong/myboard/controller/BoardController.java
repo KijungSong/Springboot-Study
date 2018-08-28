698e63de492c0988bfd5c2ea21c50afcece8c1a9
Kijung Song <skj4522@gmail.com>
2018-08-28 17:08:34 +0900

    [myboard] /writeform 요청시 SecurityContextHolder를 이용하여 UserDetails객체를 얻어와 email 출력.  - 게시판 글 작성시 작성자 이름을 표시하기위해.

--- myboard/src/main/java/com/daebaksong/myboard/controller/BoardController.java
+++ myboard/src/main/java/com/daebaksong/myboard/controller/BoardController.java
@@ -4,6 +4,9 @@ import com.daebaksong.myboard.domin.Board;
 import com.daebaksong.myboard.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -27,9 +30,17 @@ public class BoardController {
 
     @GetMapping("/writeform")
     public String writeform() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object p = authentication.getPrincipal();
+        User user = (User)p;
+        System.out.println("--------------------");
+        System.out.println(p.getClass().getName());
+        System.out.println("--------------------");
+
         return "writeform";
     }
 
+    // Principal 객체는 로그인한 사용자의 id를 가지고 있다.
     @PostMapping
     public String boards(Principal principal, @ModelAttribute Board board) {
         board.setRegdate(LocalDateTime.now());
